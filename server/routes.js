b2f264e2310b47f0873e868d219e4ac9eaf7fdf9
rsoule3 <robsoule3@gmail.com>
2015-11-17 11:09:20 -0600

    Commented out index.html serving for now

--- server/routes.js
+++ server/routes.js
@@ -22,6 +22,7 @@ module.exports = function(app) {
   // This is for supporting browser history pushstate.
   // NOTE: Make sure this route is always LAST.
   app.get('/*', function(req, res){
-    res.sendFile( assetFolder + '/www/index.html' );
+    res.status(404).send();
+    // res.sendFile( assetFolder + '/www/index.html' );
   });
 };
