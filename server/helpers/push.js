6e0f415261a83af6fafa41289f1e2293769253b2
rsoule3 <robsoule3@gmail.com>
2015-12-08 16:23:37 -0600

    working on it

--- server/helpers/push.js
+++ server/helpers/push.js
@@ -28,10 +28,11 @@ module.exports = {
             $maxDistance: 500
           }
         }
-      })
-      .select('device_id')
-      .exec(function(err, devicesToNotify) {
+      }, 'device_id', function(err, devicesToNotify) {
         if (err) { console.log('notifyUsers deviceFind ERR', err); return err; }
+        devicesToNotify = devicesToNotify.map(function(val) {
+          return val.devices_id;
+        });
         var title = (artist.artist_info.artist_name || artist.name) + 'is Live!';
         var body = 'Show your support, be a part of the experience.';
         gcmNotify(title, body, artist.artist_info, devicesToNotify);
