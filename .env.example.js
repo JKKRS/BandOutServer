753b1122357e93a2bad7b057ebdb8b6aca007b68
rsoule3 <robsoule3@gmail.com>
2015-12-08 15:26:24 -0600

    Added example .env.example.js

--- .env.example.js
+++ .env.example.js
@@ -1,5 +1,6 @@
 // create a file locally called .env.js in same directory as this, replace
 // mongoconnectionurihere
 module.exports = {
-  MONGO_COMPOSE_URI: 'mongoconnectionurihere'
+  MONGO_COMPOSE_URI: 'mongoconnectionurihere',
+  GOOGLE_SERV_API: 'isdfasdfdafsasdpfqwerqwefqwfasdflk123124123h'
 };
